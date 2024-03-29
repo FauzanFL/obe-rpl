e2787ec72b853bb178ae34d1fb2774127fdaf503
Fauzan FL <fauzanarth@gmail.com>
2024-03-29 14:29:05 +0700

    Remove console.log statement in RemoveCookie hook

--- src/hooks/RemoveCookie.js
+++ src/hooks/RemoveCookie.js
@@ -1,7 +1,6 @@
 import Cookie from 'js-cookie';
 
 const RemoveCookie = (cookiename) => {
-  console.log('Removing cookie: ', cookiename);
   Cookie.remove(cookiename, { domain: '.obe-rpl.site' });
 };
 
