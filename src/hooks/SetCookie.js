b52ef52aa090da81a5796a696517369fd998e465
Fauzan FL <fauzanarth@gmail.com>
2024-03-29 10:54:51 +0700

    Change set cookie config

--- src/hooks/SetCookie.js
+++ src/hooks/SetCookie.js
@@ -1,11 +1,13 @@
 import Cookie from 'js-cookie';
 
 const SetCookie = (cookiename, usrin) => {
+  const expires = new Date(Date.now() + 86400000);
   Cookie.set(cookiename, usrin, {
-    expires: 1,
+    expires,
     secure: true,
-    sameSite: 'None',
+    sameSite: 'Lax',
     path: '/',
+    domain: '.obe-rpl.site',
   });
 };
 
