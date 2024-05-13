6f8171bd24f9b9ee3f74dcacbd9f8f4521110149
Fauzan FL <fauzanarth@gmail.com>
2024-05-13 07:37:37 +0700

    Update search input styles in MataKuliahDosen.jsx, Clo.jsx, and LembarAssessment.jsx

--- src/pages/dosen/rps/LembarAssessment.jsx
+++ src/pages/dosen/rps/LembarAssessment.jsx
@@ -259,7 +259,7 @@ export default function LembarAssessment() {
                       type="text"
                       id="simple-search"
                       onChange={({ target }) => handleSearch(target.value)}
-                      className="bg-gray-50 w-72 max-w-96 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 w-full md:w-72 max-w-96 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block ps-10 p-2.5"
                       placeholder="Search..."
                       required
                     />
